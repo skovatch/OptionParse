df91838c789de618f46ed8a6949d4bc4e250301b
Stephen Marquis <marquissm@gmail.com>
2016-08-04 20:17:08 -0700

    Initial Commit

---
+++ OptionParse/OptionParse.h
@@ -0,0 +1,19 @@
+//
+//  OptionParse.h
+//  OptionParse
+//
+//  Created by Steve Marquis on 8/4/16.
+//  Copyright © 2016 Steve Marquis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OptionParse.
+FOUNDATION_EXPORT double OptionParseVersionNumber;
+
+//! Project version string for OptionParse.
+FOUNDATION_EXPORT const unsigned char OptionParseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptionParse/PublicHeader.h>
+
+
